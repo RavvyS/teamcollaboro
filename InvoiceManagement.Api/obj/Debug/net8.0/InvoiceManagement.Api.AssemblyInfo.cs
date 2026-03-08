f62cb8a0daf3e44b66bd37a549bc2051909b3cdf
RavvyS <156438913+RavvyS@users.noreply.github.com>
2026-03-08 14:06:54 +0530

    fix minor issues

--- InvoiceManagement.Api/obj/Debug/net8.0/InvoiceManagement.Api.AssemblyInfo.cs
+++ InvoiceManagement.Api/obj/Debug/net8.0/InvoiceManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce63bca6105942daa8914eda71869adb0bba32c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f6e70bb70988c2a4e25093d848e0f642d3620b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
